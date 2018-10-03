2c9eac613f764bb95b13a1f6aafdc7d374525c91
Kees Kluskens <kees@webduck.nl>
2018-10-03 11:28:04 +0200

    Update simulator code from Rutger

--- packages/simulator/public/index.js
+++ packages/simulator/public/index.js
@@ -4,34 +4,55 @@ let lines = [];
 const c = document.getElementById('canvas');
 const ctx = c.getContext('2d');
 const MAX_VALUE = 65535;
+const AFTERGLOW_AMOUNT = 50;
+let lastRenderTime;
+ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 ctx.strokeStyle = '#fff';
-ctx.lineWidth = 3;
+ctx.lineWidth = 2;
 
 function calculateColor(raw) {
   return Math.round((raw / MAX_VALUE) * 255);
 }
 
 function render() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const currentTime = new Date();
+  if (lastRenderTime) {
+    const frameInterval = currentTime - lastRenderTime;
+    // We add variable afterglow depending on the time until the last render.
+    ctx.fillStyle = `rgba(0, 0, 0, ${frameInterval / AFTERGLOW_AMOUNT})`;
+  }
+  lastRenderTime = currentTime;
+
+  // This rectangle will use the afterglow style from the code above.
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   lines.forEach(function(line, i) {
     if (i > 0) {
-      ctx.beginPath();
       ctx.moveTo(
         ((MAX_VALUE - (lines[i - 1].x + MAX_VALUE / 2)) / MAX_VALUE) *
           canvas.width,
         ((MAX_VALUE - (lines[i - 1].y + MAX_VALUE / 2)) / MAX_VALUE) *
           canvas.height
       );
-      ctx.lineTo(
-        ((MAX_VALUE - (line.x + MAX_VALUE / 2)) / MAX_VALUE) * canvas.width,
-        ((MAX_VALUE - (line.y + MAX_VALUE / 2)) / MAX_VALUE) * canvas.height
-      );
-      ctx.closePath();
-      ctx.strokeStyle = `rgb(${calculateColor(line.r)}, ${calculateColor(
-        line.g
-      )}, ${calculateColor(line.b)})`;
-      ctx.stroke();
+
+      // If a line doesn't have any color, it shouldn't be drawn at all. This is known as blanking.
+      if (line.r || line.g || line.b) {
+        ctx.beginPath();
+        ctx.lineTo(
+          ((MAX_VALUE - (line.x + MAX_VALUE / 2)) / MAX_VALUE) * canvas.width,
+          ((MAX_VALUE - (line.y + MAX_VALUE / 2)) / MAX_VALUE) * canvas.height
+        );
+        ctx.closePath();
+        ctx.strokeStyle = `rgb(${calculateColor(line.r)}, ${calculateColor(
+          line.g
+        )}, ${calculateColor(line.b)})`;
+        ctx.stroke();
+      } else {
+        ctx.moveTo(
+          ((MAX_VALUE - (line.x + MAX_VALUE / 2)) / MAX_VALUE) * canvas.width,
+          ((MAX_VALUE - (line.y + MAX_VALUE / 2)) / MAX_VALUE) * canvas.height
+        );
+      }
     }
   });
   requestAnimationFrame(render);
