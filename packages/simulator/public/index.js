d305fcf310751a8a76af5e5d3a35a56cf8427633
Rutger <rma.schimmel@gmail.com>
2018-10-04 16:06:13 +0200

    Simulator: change lines name to points

--- packages/simulator/public/index.js
+++ packages/simulator/public/index.js
@@ -1,6 +1,6 @@
 import WebsocketClient from './websocket.js';
 
-let lines = [];
+let points = [];
 const c = document.getElementById('canvas');
 const ctx = c.getContext('2d');
 const MAX_VALUE = 65535;
@@ -41,31 +41,31 @@ function render() {
   // This rectangle will use the afterglow style from the code above.
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  lines.forEach(function(line, i) {
+  points.forEach(function(point, i) {
     ctx.beginPath();
     if (i > 0) {
       ctx.moveTo(
-        ((MAX_VALUE - (lines[i - 1].x + MAX_VALUE / 2)) / MAX_VALUE) *
+        ((MAX_VALUE - (points[i - 1].x + MAX_VALUE / 2)) / MAX_VALUE) *
           canvas.width,
-        ((MAX_VALUE - (lines[i - 1].y + MAX_VALUE / 2)) / MAX_VALUE) *
+        ((MAX_VALUE - (points[i - 1].y + MAX_VALUE / 2)) / MAX_VALUE) *
           canvas.height
       );
     }
 
-    // If a line doesn't have any color, it shouldn't be drawn at all. This is known as blanking.
-    if (line.r || line.g || line.b) {
+    // If a point doesn't have any color, it shouldn't be drawn at all. This is known as blanking.
+    if (point.r || point.g || point.b) {
       ctx.lineTo(
-        ((MAX_VALUE - (line.x + MAX_VALUE / 2)) / MAX_VALUE) * canvas.width,
-        ((MAX_VALUE - (line.y + MAX_VALUE / 2)) / MAX_VALUE) * canvas.height
+        ((MAX_VALUE - (point.x + MAX_VALUE / 2)) / MAX_VALUE) * canvas.width,
+        ((MAX_VALUE - (point.y + MAX_VALUE / 2)) / MAX_VALUE) * canvas.height
       );
-      ctx.strokeStyle = `rgb(${calculateColor(line.r)}, ${calculateColor(
-        line.g
-      )}, ${calculateColor(line.b)})`;
+      ctx.strokeStyle = `rgb(${calculateColor(point.r)}, ${calculateColor(
+        point.g
+      )}, ${calculateColor(point.b)})`;
       ctx.stroke();
     } else {
       ctx.moveTo(
-        ((MAX_VALUE - (line.x + MAX_VALUE / 2)) / MAX_VALUE) * canvas.width,
-        ((MAX_VALUE - (line.y + MAX_VALUE / 2)) / MAX_VALUE) * canvas.height
+        ((MAX_VALUE - (point.x + MAX_VALUE / 2)) / MAX_VALUE) * canvas.width,
+        ((MAX_VALUE - (point.y + MAX_VALUE / 2)) / MAX_VALUE) * canvas.height
       );
     }
     ctx.closePath();
@@ -78,5 +78,5 @@ const host = window.document.location.host.replace(/:.*/, '');
 const ws = new WebsocketClient();
 ws.open('ws://' + host + ':8080');
 ws.onmessage = function(event) {
-  lines = JSON.parse(event.data);
+  points = JSON.parse(event.data);
 };
