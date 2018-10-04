b91aa9ccf6e4bfb4677b4e364f3d889b9ea4cb75
Rutger <rma.schimmel@gmail.com>
2018-10-04 17:33:18 +0200

    Simulator: make rendering more efficient

--- packages/simulator/public/index.js
+++ packages/simulator/public/index.js
@@ -1,9 +1,10 @@
 import WebsocketClient from './websocket.js';
 
 let points = [];
-const c = document.getElementById('canvas');
-const ctx = c.getContext('2d');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const MAX_VALUE = 65535;
+const HALF_MAX_VALUE = MAX_VALUE / 2;
 const AFTERGLOW_AMOUNT = 50;
 let lastRenderTime;
 ctx.strokeStyle = '#fff';
@@ -13,8 +14,8 @@ function handleResize() {
   const pixelRatio = window.devicePixelRatio;
 
   ctx.scale(pixelRatio, pixelRatio);
-  c.width = Math.floor(c.clientWidth * pixelRatio);
-  c.height = Math.floor(c.clientHeight * pixelRatio);
+  canvas.width = Math.floor(canvas.clientWidth * pixelRatio);
+  canvas.height = Math.floor(canvas.clientHeight * pixelRatio);
   ctx.lineWidth = pixelRatio;
 }
 handleResize();
@@ -24,6 +25,10 @@ window.onresize = handleResize;
 window
   .matchMedia('screen and (min-resolution: 2dppx)')
   .addListener(handleResize);
+
+function calculateRelativePosition(position) {
+  return (position + HALF_MAX_VALUE) / MAX_VALUE;
+}
 
 function calculateColor(raw) {
   return Math.round((raw / MAX_VALUE) * 255);
@@ -44,17 +49,18 @@ function render() {
   points.forEach(function(point, i) {
     ctx.beginPath();
     if (i > 0) {
+      const previousPoint = points[i - 1];
       ctx.moveTo(
-        ((points[i - 1].x + MAX_VALUE / 2) / MAX_VALUE) * canvas.width,
-        ((points[i - 1].y + MAX_VALUE / 2) / MAX_VALUE) * canvas.height
+        calculateRelativePosition(previousPoint.x) * canvas.width,
+        calculateRelativePosition(previousPoint.y) * canvas.height
       );
     }
 
     // If a point doesn't have any color, it shouldn't be drawn at all. This is known as blanking.
     if (point.r || point.g || point.b) {
       ctx.lineTo(
-        ((point.x + MAX_VALUE / 2) / MAX_VALUE) * canvas.width,
-        ((point.y + MAX_VALUE / 2) / MAX_VALUE) * canvas.height
+        calculateRelativePosition(point.x) * canvas.width,
+        calculateRelativePosition(point.y) * canvas.height
       );
       ctx.strokeStyle = `rgb(${calculateColor(point.r)}, ${calculateColor(
         point.g
@@ -62,8 +68,8 @@ function render() {
       ctx.stroke();
     } else {
       ctx.moveTo(
-        ((point.x + MAX_VALUE / 2) / MAX_VALUE) * canvas.width,
-        ((point.y + MAX_VALUE / 2) / MAX_VALUE) * canvas.height
+        calculateRelativePosition(point.x) * canvas.width,
+        calculateRelativePosition(point.y) * canvas.height
       );
     }
     ctx.closePath();
