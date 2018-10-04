f4571c2cfd02fcafb876485ad3c46a881e8471d3
Rutger <rma.schimmel@gmail.com>
2018-10-04 20:11:09 +0200

    Improve MAX_VALUE documentation

--- packages/draw/src/constants.ts
+++ packages/draw/src/constants.ts
@@ -1,5 +1,7 @@
-// TODO: make this name more descriptive
-// TODO: add documentation as to why this specific number is the max
+// The Ether Dream firmware uses 16-bit integers for all variables, ranging from 0 to 65535.
+// We need this number as the correct scale values for many parameters.
+// Source: https://github.com/j4cbo/j4cDAC/blob/e592ebcb7c9b6fb521be2005f4b85de54bc04f0f/common/protocol.h
 export const MAX_VALUE = 65535;
+
 // TODO: find out what this does exactly
 export const RESOLUTION = 150;
