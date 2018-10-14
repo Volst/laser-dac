709a408d1c453acb29c2dc8ed97592d4d8005316
Kees Kluskens <kees@webduck.nl>
2018-10-14 16:43:16 +0200

    Add basic bezier and arc tests

--- packages/draw/src/__tests__/Path.ts
+++ packages/draw/src/__tests__/Path.ts
@@ -21,4 +21,33 @@ test('draw a triangle', () => {
   expect(triangle.draw(10)).toMatchSnapshot();
 });
 
-// TODO: draw something with an x,y offset
+test('draw a triangle with x/y offset', () => {
+  const triangle = new Path({
+    path: 'M0.67 0 l0.33 0.88 L1 0.88 Z',
+    color: [0, 1, 0],
+    x: 0.1,
+    y: 0.2
+  });
+  expect(triangle.draw(10)).toMatchSnapshot();
+});
+
+test('draw a smile (curves)', () => {
+  const smile = new Path({
+    path: 'M0.5,0.8c0,0,0,0.1,0.1,0.1c0.1,0,0.1-0.1,0.1-0.1',
+    color: [0, 1, 0],
+    x: 0,
+    y: 0
+  });
+  expect(smile.draw(10)).toMatchSnapshot();
+});
+
+test('draw an arc', () => {
+  const arc = new Path({
+    path:
+      'M10 315L 110 215A 30 50 0 0 1 162.55 162.45L 172.55 152.45A 30 50 -45 0 1 215.1 109.9L 315 10',
+    color: [0, 1, 0],
+    width: 320,
+    height: 320
+  });
+  expect(arc.draw(10)).toMatchSnapshot();
+});
