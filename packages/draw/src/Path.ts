5517c83c20b1415487c1bd7d9f6deef0c475a44e
Rutger <rma.schimmel@gmail.com>
2018-10-18 17:25:25 +0200

    Draw: fix types

--- packages/draw/src/Path.ts
+++ packages/draw/src/Path.ts
@@ -87,161 +87,164 @@ export class Path extends Shape {
     let prevX = 0;
     let prevY = 0;
 
-    const points = pathData.commands.reduce((accumulator: [], command) => {
-      let commandPoints: any[] = [];
+    const points = pathData.commands.reduce(
+      (accumulator: Point[], command: SVGCommand) => {
+        let commandPoints: Point[] = [];
 
-      switch (command.type) {
-        case SVGPathData.MOVE_TO:
-          commandPoints = new Wait({
-            x: command.x,
-            y: command.y,
-            amount: BLANKING_AMOUNT
-          }).draw();
-
-          lastMoveCommand = command;
-          prevX = command.x;
-          prevY = command.y;
-          break;
-
-        case SVGPathData.HORIZ_LINE_TO:
-        case SVGPathData.VERT_LINE_TO:
-        case SVGPathData.LINE_TO:
-          const toX = 'x' in command ? command.x : prevX;
-          const toY = 'y' in command ? command.y : prevY;
-          commandPoints = new Line({
-            from: { x: prevX, y: prevY },
-            to: { x: toX, y: toY },
-            color: this.color
-          }).draw(resolution);
-          prevX = toX;
-          prevY = toY;
-          break;
-
-        case SVGPathData.CURVE_TO:
-          commandPoints = new CubicCurve({
-            from: {
-              x: prevX,
-              y: prevY,
-              control: { x: command.x1, y: command.y1 }
-            },
-            to: {
+        switch (command.type) {
+          case SVGPathData.MOVE_TO:
+            commandPoints = new Wait({
               x: command.x,
               y: command.y,
-              control: { x: command.x2, y: command.y2 }
-            },
-            color: this.color
-          }).draw(resolution);
-          prevX = command.x;
-          prevY = command.y;
-          break;
+              amount: BLANKING_AMOUNT
+            }).draw();
 
-        case SVGPathData.QUAD_TO:
-          commandPoints = new QuadCurve({
-            from: { x: prevX, y: prevY },
-            to: { x: command.x, y: command.y },
-            control: { x: command.x1, y: command.y1 },
-            color: this.color
-          }).draw(resolution);
-          prevX = command.x;
-          prevY = command.y;
-          break;
+            lastMoveCommand = command;
+            prevX = command.x;
+            prevY = command.y;
+            break;
 
-        case SVGPathData.ARC:
-          // TODO: Of course it would be better to implement this properly instead of converting arcs to a cubic bezier.
-          const curves = arcToBezier({
-            px: prevX,
-            py: prevY,
-            cx: command.x,
-            cy: command.y,
-            rx: command.rX,
-            ry: command.rY,
-            xAxisRotation: command.xRot,
-            largeArcFlag: command.lArcFlag,
-            sweepFlag: command.sweepFlag
-          });
-          let curvePrevX = prevX;
-          let curvePrevY = prevY;
-          curves.forEach(curve => {
-            const curvePoints = new CubicCurve({
+          case SVGPathData.HORIZ_LINE_TO:
+          case SVGPathData.VERT_LINE_TO:
+          case SVGPathData.LINE_TO:
+            const toX = 'x' in command ? command.x : prevX;
+            const toY = 'y' in command ? command.y : prevY;
+            commandPoints = new Line({
+              from: { x: prevX, y: prevY },
+              to: { x: toX, y: toY },
+              color: this.color
+            }).draw(resolution);
+            prevX = toX;
+            prevY = toY;
+            break;
+
+          case SVGPathData.CURVE_TO:
+            commandPoints = new CubicCurve({
               from: {
-                x: curvePrevX,
-                y: curvePrevY,
-                control: { x: curve.x1, y: curve.y1 }
+                x: prevX,
+                y: prevY,
+                control: { x: command.x1, y: command.y1 }
               },
               to: {
-                x: curve.x,
-                y: curve.y,
-                control: { x: curve.x2, y: curve.y2 }
+                x: command.x,
+                y: command.y,
+                control: { x: command.x2, y: command.y2 }
               },
               color: this.color
             }).draw(resolution);
-            curvePrevX = curve.x;
-            curvePrevY = curve.y;
-            Array.prototype.push.apply(commandPoints, curvePoints);
-          });
-          prevX = command.x;
-          prevY = command.y;
-          break;
+            prevX = command.x;
+            prevY = command.y;
+            break;
 
-        case SVGPathData.CLOSE_PATH:
-          if (!lastMoveCommand) {
-            throw new Error(
-              'Path parsing error: close path command called without a prior move command.'
+          case SVGPathData.QUAD_TO:
+            commandPoints = new QuadCurve({
+              from: { x: prevX, y: prevY },
+              to: { x: command.x, y: command.y },
+              control: { x: command.x1, y: command.y1 },
+              color: this.color
+            }).draw(resolution);
+            prevX = command.x;
+            prevY = command.y;
+            break;
+
+          case SVGPathData.ARC:
+            // TODO: Of course it would be better to implement this properly instead of converting arcs to a cubic bezier.
+            const curves = arcToBezier({
+              px: prevX,
+              py: prevY,
+              cx: command.x,
+              cy: command.y,
+              rx: command.rX,
+              ry: command.rY,
+              xAxisRotation: command.xRot,
+              largeArcFlag: command.lArcFlag,
+              sweepFlag: command.sweepFlag
+            });
+            let curvePrevX = prevX;
+            let curvePrevY = prevY;
+            curves.forEach(curve => {
+              const curvePoints = new CubicCurve({
+                from: {
+                  x: curvePrevX,
+                  y: curvePrevY,
+                  control: { x: curve.x1, y: curve.y1 }
+                },
+                to: {
+                  x: curve.x,
+                  y: curve.y,
+                  control: { x: curve.x2, y: curve.y2 }
+                },
+                color: this.color
+              }).draw(resolution);
+              curvePrevX = curve.x;
+              curvePrevY = curve.y;
+              Array.prototype.push.apply(commandPoints, curvePoints);
+            });
+            prevX = command.x;
+            prevY = command.y;
+            break;
+
+          case SVGPathData.CLOSE_PATH:
+            if (!lastMoveCommand) {
+              throw new Error(
+                'Path parsing error: close path command called without a prior move command.'
+              );
+            }
+            commandPoints = new Line({
+              from: { x: prevX, y: prevY },
+              to: { x: lastMoveCommand.x, y: lastMoveCommand.y },
+              color: this.color
+            }).draw(resolution);
+            prevX = lastMoveCommand.x;
+            prevY = lastMoveCommand.y;
+            break;
+
+          default:
+            console.warn(
+              `Path parsing warning: command ${command.type} is not supported`
             );
-          }
-          commandPoints = new Line({
-            from: { x: prevX, y: prevY },
-            to: { x: lastMoveCommand.x, y: lastMoveCommand.y },
-            color: this.color
-          }).draw(resolution);
-          prevX = lastMoveCommand.x;
-          prevY = lastMoveCommand.y;
-          break;
+        }
 
-        default:
-          console.warn(
-            `Path parsing warning: command ${command.type} is not supported`
+        let wait: Point[] = [];
+        if (accumulator.length >= 2 && commandPoints.length) {
+          const nextPoint: Point = commandPoints[0];
+          const lastPoint: Point = accumulator[accumulator.length - 1];
+          const secondLastPoint: Point = accumulator[accumulator.length - 2];
+
+          // Get previous angle in radians.
+          const previousAngle = Math.atan2(
+            secondLastPoint.y - lastPoint.y,
+            secondLastPoint.x - lastPoint.x
           );
-      }
 
-      let wait: Point[] = [];
-      if (accumulator.length >= 2 && commandPoints.length) {
-        const nextPoint: Point = commandPoints[0];
-        const lastPoint: Point = accumulator[accumulator.length - 1];
-        const secondLastPoint: Point = accumulator[accumulator.length - 2];
+          // Get next angle in radians.
+          const nextAngle = Math.atan2(
+            lastPoint.y - nextPoint.y,
+            lastPoint.x - nextPoint.x
+          );
 
-        // Get previous angle in radians.
-        const previousAngle = Math.atan2(
-          secondLastPoint.y - lastPoint.y,
-          secondLastPoint.x - lastPoint.x
-        );
+          // Get difference in angle where 90 degrees is 0.5, and 180 is 1.
+          const relativeAngle =
+            Math.abs(
+              Math.atan2(
+                Math.sin(previousAngle - nextAngle),
+                Math.cos(previousAngle - nextAngle)
+              )
+            ) / Math.PI;
 
-        // Get next angle in radians.
-        const nextAngle = Math.atan2(
-          lastPoint.y - nextPoint.y,
-          lastPoint.x - nextPoint.x
-        );
+          const waitShape = new Wait({
+            x: positionToRelative(lastPoint.x),
+            y: positionToRelative(lastPoint.y),
+            color: [lastPoint.r, lastPoint.g, lastPoint.b],
+            amount: Math.floor(MAX_WAIT_AMOUNT * relativeAngle)
+          });
+          wait = waitShape.draw();
+        }
 
-        // Get difference in angle where 90 degrees is 0.5, and 180 is 1.
-        const relativeAngle =
-          Math.abs(
-            Math.atan2(
-              Math.sin(previousAngle - nextAngle),
-              Math.cos(previousAngle - nextAngle)
-            )
-          ) / Math.PI;
-
-        const waitShape = new Wait({
-          x: positionToRelative(lastPoint.x),
-          y: positionToRelative(lastPoint.y),
-          color: [lastPoint.r, lastPoint.g, lastPoint.b],
-          amount: Math.floor(MAX_WAIT_AMOUNT * relativeAngle)
-        });
-        wait = waitShape.draw();
-      }
-
-      return [...accumulator, ...wait, ...commandPoints];
-    }, []);
+        return [...accumulator, ...wait, ...commandPoints];
+      },
+      []
+    );
 
     return flatten(points) as Point[];
   }
