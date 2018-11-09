22a4d9e98fee4d9917c6d9778b558bc11b920187
Kees Kluskens <kees@webduck.nl>
2018-11-09 16:40:22 +0100

    Fix tests

--- packages/draw/src/__tests__/Scene.ts
+++ packages/draw/src/__tests__/Scene.ts
@@ -15,11 +15,23 @@ class MyShape extends Shape {
   }
 }
 
-test('draw two shapes on the scene', () => {
+test('draw a shape on the scene', () => {
   const scene = new Scene();
   const shapeA = new MyShape({ x: 0.1, y: 0.1 });
-  const shapeB = new MyShape({ x: 0.5, y: 0.5 });
-  scene.add(shapeA, shapeB);
+  scene.add(shapeA);
+
+  expect(scene.points).toMatchSnapshot();
+});
+
+function transformer(points: Point[]) {
+  points.forEach(point => (point.x += 10));
+  return points;
+}
+
+test('draw a shape and use a transformer', () => {
+  const scene = new Scene();
+  const shapeA = new MyShape({ x: 0.1, y: 0.1 });
+  scene.add(shapeA, transformer);
 
   expect(scene.points).toMatchSnapshot();
 });
