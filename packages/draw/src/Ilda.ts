a9e48cdf450b7cf9272458c4d44ed4e7ac5634ef
Elze Kool <efrkool@live.nl>
2019-06-02 11:07:32 +0200

    Allow color override for Ilda and IldaFont

--- packages/draw/src/Ilda.ts
+++ packages/draw/src/Ilda.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import { fromByteArray, Section } from '@laser-dac/ilda-reader';
 import { Shape } from './Shape';
+import { Color } from './Point';
 
 export interface File {
   sections: Section[];
@@ -10,6 +11,7 @@ interface IldaOptions {
   x?: number;
   y?: number;
   size?: number;
+  color?: Color;
   frame: number;
   file: File;
 }
@@ -18,6 +20,7 @@ export class Ilda extends Shape {
   x?: number;
   y?: number;
   size?: number;
+  color?: Color;
   frame: number;
   file: File;
 
@@ -26,13 +29,17 @@ export class Ilda extends Shape {
     this.x = options.x;
     this.y = options.y;
     this.size = options.size;
+    this.color = options.color;
     this.frame = options.frame;
     this.file = options.file;
   }
 
   isStatic() {
     return (
-      this.x === undefined && this.y === undefined && this.size === undefined
+      this.x === undefined &&
+      this.y === undefined &&
+      this.size === undefined &&
+      this.color === undefined
     );
   }
 
@@ -44,14 +51,17 @@ export class Ilda extends Shape {
     }
     const x = this.x || 0;
     const y = this.y || 0;
+    const color = this.color;
     const size = this.size || 1;
+
     return section.points.map(point => {
+      const isBlank = point.r === 0 && point.g === 0 && point.b === 0;
       return {
         x: x + point.x * size,
         y: y + point.y * size,
-        r: point.r,
-        g: point.g,
-        b: point.b
+        r: color !== undefined && !isBlank ? color[0] : point.r,
+        g: color !== undefined && !isBlank ? color[1] : point.g,
+        b: color !== undefined && !isBlank ? color[2] : point.b
       };
     });
   }
