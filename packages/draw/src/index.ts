00524f8b7ac63142b81f0bff3a35c1dfd2ba4de8
Elze Kool <efrkool@live.nl>
2019-06-09 12:39:44 +0200

    Replace Ilda Font with Hershey font
    Fixes: #3

--- packages/draw/src/index.ts
+++ packages/draw/src/index.ts
@@ -4,7 +4,7 @@ export { Rect } from './Rect';
 export { Circle } from './Circle';
 export { Path } from './Path';
 export { Ilda, loadIldaFile } from './Ilda';
-export { IldaFont } from './IldaFont';
+export { HersheyFont, loadHersheyFont } from './HersheyFont';
 export { Timeline } from './Timeline';
 export { rotate } from './transformers/rotate';
 export { distort } from './transformers/distort';
