9956f8e5a29acb762348ae95c92677abaa4e18f4
Sterling Hirsh <mybadluck22@gmail.com>
2021-01-18 15:53:42 -0600

    Drop unused RESOLUTION constant.

--- packages/draw/src/constants.ts
+++ packages/draw/src/constants.ts
@@ -1,6 +1,3 @@
-// TODO: find out what this does exactly
-export const RESOLUTION = 150;
-
 export const BLANKING_AMOUNT = 24;
 
 // This value determines how many times a Wait-point should be applied for the sharpest possible angles (360 degree angles).
